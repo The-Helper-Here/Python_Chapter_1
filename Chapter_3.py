f655ca1dce4fde376540968a33904141f0035928
The Helper Here <76590967+The-Helper-Here@users.noreply.github.com>
2020-12-26 19:20:47 +0530

    Chapter_3.py

---
+++ Chapter_3.py
@@ -0,0 +1,78 @@
+
+'''
+Chapter 2 - Comments, Escape Sequences & Print Statement in detail.
+------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------
+
+COMMENTS IN PYTHON.
+
+What are comments in Python?
+                            Comments are used to write something which the programmer does not want to
+execute. Comments can be written to mark author name, date when the program is written, adding notes 
+for your future self, etc.
+There are two types of Comments in Python:
+1.Single Line Comment: Single Line comments are the comments which are written in a single line, i.e.,
+                       they occupy the space of a single line.They start with a hash/pound(#).
+Example:'''
+#This is a single line comment.
+'''
+2.Multi line Comments:  Multi-Line comments are the comments which are created by using multiple lines,
+                        i.e., they occupy more than one line in a program.They are enclosed in Triple quotation.
+Example:'''
+
+'''
+This Is 
+A Mutli Line
+Comment
+'''
+
+'''
+------------------------------------------------------------------------------------------------------
+
+ESCAPE SEQUENCE IN PYTHON.
+
+What is escape sequence in Python?
+                                 An Escape Sequence character in Python is a sequence of characters 
+that represents a single character.It doesn't represent itself when used inside string literal or 
+character.It is composed of two or more characters starting with backslash \ but acts as a single character.
+Example:'''
+
+print("Hello \n world")
+
+'''
+Some commonly used escape sequence charecter:
+                                             \'	Single Quote
+                                             \"  Double Quote	
+                                             \\	Backslash	
+                                             \v  Vertical Tab 
+                                             \n	New Line	
+                                             \r	Carriage Return	
+                                             \t	Tab	
+                                             \b	Backspace	
+                                             \f	Form Feed
+
+------------------------------------------------------------------------------------------------------
+
+PRINT STATEMENT IN DETAIL.
+
+What is Print statement in Python?
+                                  print() is a function in Python that allows us to display whatever 
+is written inside it. In case an operation is supplied to print, the value of the expression after the
+evaluation is printed in the terminal.
+Example:'''
+
+print("Hello world!")
+
+'''      
+End argument:
+            End argument allows us to put something at the end of the line after it is printed.
+Exaple:'''
+
+print("Hello", end="world")
+
+'''
+------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------
+End of chapter 3
+Author: The Helper Here </>
+'''
